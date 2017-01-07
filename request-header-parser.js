3ff8e5805ba45d9ab0b7ddf2021b2408e1285574
Pauric Holleran <pauric.holleran@gmail.com>
2017-01-07 18:02:40 +0000

    IP address, language and operating system functionality

--- request-header-parser.js
+++ request-header-parser.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var app = express();
+var useragent = require('useragent');
+var parser = require('accept-language-parser');
 var ip;
-var ua;
-
-
+var operatingSystem;
+var language;
+var whoAmIJSON;
 
 app.get('/*', function(req, res) {
-  
-  
-  res.locals.ua = req.get('User-Agent');
-  
+
   ip = req.connection.remoteAddress;
-  ua = req.get('User-Agent');
+  operatingSystem = useragent.parse(req.headers['user-agent']);
+  language = parser.parse(req.headers["accept-language"]);
+  language = language[0].code + '-' + language[0].region;
   
-  res.send(ip + '\n' + ua);
+  whoAmIJSON = JSON.stringify({
+    "ipaddress": ip,
+    "language": language,
+    "software": operatingSystem.os.family
+  });
+  
+
+  res.send(whoAmIJSON);
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
